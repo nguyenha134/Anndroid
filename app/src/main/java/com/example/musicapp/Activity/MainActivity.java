020e031ed2f28556064c75b7f4e2c96cfa564047
Nguyen Ha <nguyenhoangha1342001@gmail.com>
2022-03-05 20:09:17 +0700

    BTLAndroid

---
+++ app/src/main/java/com/example/musicapp/Activity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.musicapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.musicapp.Adapter.MainViewPagerAdapter;
+import com.example.musicapp.Fragment.Fragment_Tim_Kiem;
+import com.example.musicapp.Fragment.Fragment_Trang_Chu;
+import com.example.musicapp.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        anhxa();
+        init();
+    }
+
+    private void init(){
+        MainViewPagerAdapter mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
+        mainViewPagerAdapter.addFragment(new Fragment_Trang_Chu(), "Trang chủ");
+        mainViewPagerAdapter.addFragment(new Fragment_Tim_Kiem(), "Tìm kiếm");
+        viewPager.setAdapter(mainViewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.icontrangchu);
+        tabLayout.getTabAt(1).setIcon(R.drawable.icontimkiem);
+    }
+    private void anhxa(){
+        tabLayout = findViewById(R.id.myTabLayout);
+        viewPager = findViewById(R.id.myViewPager);
+    }
+}
